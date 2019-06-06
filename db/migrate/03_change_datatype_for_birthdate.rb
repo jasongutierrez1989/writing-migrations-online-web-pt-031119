abbfb91d34d9527521912e438b7e084c8f3bbc80
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-06 02:55:05 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    change_column :students do |t|
-      t.datetime :birthdate
+    change_column :students, :birthdate, datetime
     end
   end
 end
